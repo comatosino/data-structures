64373b3295f290b06b59f6df6ac40747d6d9c9e4
Robert Adams <adamsiii.robert@gmail.com>
2022-01-07 17:09:21 -0800

    added DoublyLinkedList and ListNode classes

---
+++ DoublyLinkedList/DoublyLinkedList.js
@@ -0,0 +1,8 @@
+export default class DoublyLinkedList {
+    constructor(head) {
+      this.head = head; // first node in the list
+    }
+
+    // methods
+
+}
---
+++ DoublyLinkedList/ListNode.js
@@ -0,0 +1,10 @@
+export default class ListNode {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+      this.prev = null;
+    }
+
+    // methods
+
+}
