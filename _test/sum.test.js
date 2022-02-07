e51e298daff6f7d7ff0ac014df997941b36a3d9e
Robert Adams <adamsiii.robert@gmail.com>
2022-02-06 20:16:55 -0800

    deleted demo file

--- _test/sum.test.js
+++
@@ -1,8 +0,0 @@
-const sum = require('../_dist/sum');
-
-// test()
-// 1st arg: desc of the test
-// 2nd arg: callback test function
-test('properly adds two numbers', () => {
-  expect(sum(1,2)).toBe(3);
-});
