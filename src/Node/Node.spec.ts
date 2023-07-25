9279d5c7f1f16d394049f3a61f26aa12998884c7
Robert Adams <adamsiii.robert@gmail.com>
2023-07-25 11:39:26 -0700

    update test descriptions

--- src/Node/Node.spec.ts
+++ src/Node/Node.spec.ts
@@ -4,7 +4,7 @@ import { expect } from "chai";
 import { SinglyLinkedNode, DoublyLinkedNode } from ".";
 
 describe("Singly Linked Node class", function () {
-  it("instantiates a new Singly Linked Node with given data", function () {
+  it("creates a new singly linked node with given data", function () {
     const data = 42;
     const node = new SinglyLinkedNode(data);
 
@@ -14,7 +14,7 @@ describe("Singly Linked Node class", function () {
 });
 
 describe("Doubly Linked Node class", function () {
-  it("instantiates a new Doubly Linked Node with given data", function () {
+  it("creates a new doubly linked node with given data", function () {
     const data = 42;
     const node = new DoublyLinkedNode(data);
 
