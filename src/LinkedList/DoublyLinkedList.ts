7c71a22e7985e07bbd8a2ca3d7f41861933e44ea
Robert Adams <adamsiii.robert@gmail.com>
2023-07-29 16:29:30 -0700

    add private helper, add get methods, add indexOf

--- src/LinkedList/DoublyLinkedList.ts
+++ src/LinkedList/DoublyLinkedList.ts
@@ -6,8 +6,6 @@ interface INode<T> {
 
 /**
  * Doubly Linked List implementation
- *
- * Emulates behavior of Java 8 LinkedList class
  */
 export class LinkedList<T> {
   #_Node = class Node<T> implements INode<T> {
@@ -36,11 +34,39 @@ export class LinkedList<T> {
   }
 
   /**
+   * Private helper to return node at given index
+   * @param index index of the node to return
+   * @returns reference to the node at given index
+   * @throws Error if index not of type number
+   * @throws Error if index index < 0 || index >= size
+   */
+  #_get(index: number) {
+    if (typeof index !== 'number') {
+      throw new Error('invalid index type');
+    }
+    if (index < 0 || index >= this.#_size) {
+      throw new Error('index out of bounds');
+    }
+    let n = this.#_head;
+    let i = 0;
+    while (i <= index) {
+      if (i === index) {
+        return n;
+      }
+      n = n!.next;
+      i++;
+    }
+    return null;
+  }
+
+  /**
    * Inserts the specified element at the specified position in this list.
    * Shifts the element currently at that position (if any) and any subsequent elements to the right (adds one to their indices).
    * If index not given, adds element to end of list
    * @param element element to be inserted
    * @param index index at which the specified element is to be inserted
+   * @throws Error if index given but not of type number
+   * @throws Error if index out of bounds
    */
   public add(element: T, index?: number) {
     if (index !== undefined && typeof index !== 'number') {
@@ -50,22 +76,19 @@ export class LinkedList<T> {
       throw new Error('index out of bounds');
     }
     if (index === 0) {
-      return this.addFirst(element);
+      return this.addLast(element);
     }
     if (this.#_head === null || index === undefined || index === this.#_size) {
       return this.addLast(element);
     }
-    let i = 0;
-    let n: INode<T> | null = this.#_head;
-    while (i < index - 1) {
-      n = n!.next!;
-      i++;
-    }
+    const n = this.#_get(index - 1);
     const node = new this.#_Node(element);
-    node.next = n.next;
+
+    node.next = n!.next;
     node.prev = n;
-    n.next!.prev = node;
-    n.next = node;
+    n!.next!.prev = node;
+    n!.next = node;
+
     this.#_size++;
   }
 
@@ -114,11 +137,14 @@ export class LinkedList<T> {
   }
 
   /**
-   * Returns a shallow copy of this LinkedList.
+   * Returns a copy of this LinkedList.
    * @param deep if true, returns a deep copy of this LinkedList
+   * @returns by default, a shallow copy of this LinkedList
    */
   clone(deep = false) {
     const copy = new LinkedList();
+    // deep copy -> add new nodes
+    // shallow copy -> use these nodes
     if (deep) {
       let n = this.#_head;
       while (n !== null) {
@@ -150,10 +176,59 @@ export class LinkedList<T> {
 
   /**
    * Retrieves, but does not remove, the head (first element) of this list.
-   * @returns the head of this list; null if list is empty
+   * @returns the data at the head of this list, or null if the list is empty
    */
   public element() {
     return this.#_head ? this.#_head.data : null;
+  }
+
+  /**
+   * Returns the element at the specified position in this list.
+   * @param index index of the element to return
+   * @returns the element at the specified position in this list
+   * @throws Error if index not of type number
+   * @throws Error if index out of bounds
+   */
+  public get(index: number) {
+    const n = this.#_get(index);
+    return n ? n.data : null;
+  }
+
+  /**
+   * @returns the first element in this list.
+   */
+  public getFirst() {
+    return this.#_head ? this.#_head.data : null;
+  }
+
+  /**
+   * @returns the last element in this list.
+   */
+  public getlast() {
+    return this.#_tail ? this.#_tail.data : null;
+  }
+
+  /**
+   * @returns index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+   */
+  public indexOf(element: T) {
+    let n = this.#_head;
+    let i = 0;
+    while (n !== null) {
+      if (n.data === element) {
+        return i;
+      }
+      n = n.next;
+      i++;
+    }
+    return -1;
+  }
+
+  /**
+   *
+   */
+  public lastIndexOf() {
+    //
   }
 
   public toArray() {
