913adac44a429b1cf14dbab87c6b29d67de93e24
Robert Adams <adamsiii.robert@gmail.com>
2023-07-28 22:20:24 -0700

    decouple class defs

--- src/Node/Node.ts
+++ src/Node/Node.ts
@@ -2,6 +2,8 @@ export class SinglyLinkedNode<T> {
   public next: SinglyLinkedNode<T> | null = null;
   constructor(public data: T) {}
 }
-export class DoublyLinkedNode<T> extends SinglyLinkedNode<T> {
+export class DoublyLinkedNode<T> {
+  public next: DoublyLinkedNode<T> | null = null;
   public prev: DoublyLinkedNode<T> | null = null;
+  constructor(public data: T) {}
 }
