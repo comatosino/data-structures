69ccb4f6af61d70a391a5257040a726796702685
Robert Adams <r0b4dams@gmail.com>
2024-05-05 17:10:05 -0700

    Simplify queue

--- data_structures/queue/__init__.py
+++ data_structures/queue/__init__.py
@@ -1,2 +1,37 @@
-from .queue import Queue
-from .two_stack_queue import TwoStackQueue
+class QueueNode:
+    def __init__(self, data):
+        self.data = data
+        self.next: QueueNode = None
+
+
+class Queue:
+    """FIFO"""
+
+    def __init__(self):
+        self.head: QueueNode = None
+        self.tail: QueueNode = None
+
+    def isempty(self) -> bool:
+        return self.head is None
+
+    def peek(self):
+        if not self.head:
+            return None
+        return self.head.data
+
+    def add(self, data):
+        node = QueueNode(data)
+        if self.tail is not None:
+            self.tail.next = node
+        self.tail = node
+        if self.head is None:
+            self.head = node
+
+    def remove(self):
+        if self.head is None:
+            return None
+        data = self.head.data
+        self.head = self.head.next
+        if self.head is None:
+            self.tail = None
+        return data
