0e37292cf155eb388654b79b1f72ae04bc7792b3
Robert Adams <r0b4dams@gmail.com>
2024-06-26 14:05:23 -0700

    Add iterative & level traversals

--- data_structures/tree/__init__.py
+++ data_structures/tree/__init__.py
@@ -1,56 +1,76 @@
-class TreeNode:
-    """BST"""
+from typing import Deque
+from collections import deque
 
+
+class BinaryTree:
     def __init__(self, data):
-        self.left: TreeNode = None
-        self.right: TreeNode = None
+        self.left: BinaryTree = None
+        self.right: BinaryTree = None
         self.data = data
 
     def insert(self, value):
         if value <= self.data:
             if not self.left:
-                self.left = TreeNode(value)
+                self.left = BinaryTree(value)
             else:
                 self.left.insert(value)
         else:
             if not self.right:
-                self.right = TreeNode(value)
+                self.right = BinaryTree(value)
             else:
                 self.right.insert(value)
 
     def contains(self, value):
         if value == self.data:
             return True
-
-        elif value < self.data:
-            if not self.left:
-                return False
-            else:
-                return self.left.contains(value)
-
-        else:
-            if not self.right:
-                return False
-            else:
-                return self.right.contains(value)
-
-    def print_in_order(self):
         if self.left:
-            self.left.print_in_order()
+            return self.left.contains(value)
+        if self.right:
+            return self.right.contains(value)
+        return False
+
+    def traverse_in_order_iterative(self):
+        curr: BinaryTree = self
+        stack = []
+        while True:
+            if curr is not None:
+                stack.append(curr)
+                curr = curr.left
+            elif stack:
+                curr = stack.pop()
+                print(curr.data)
+                curr = curr.right
+            else:
+                break
+
+    def level_order(self):
+        queue: Deque[BinaryTree] = deque()
+        queue.append(self)
+        while queue:
+            node = queue.popleft()
+            print(node.data)
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+
+    def inorder(self):
+        if self.left:
+            self.left.inorder()
         print(self.data)
         if self.right:
-            self.right.print_in_order()
+            self.right.inorder()
 
-    def print_preorder(self):
+    def preorder(self):
         print(self.data)
         if self.left:
-            self.left.print_preorder()
+            self.left.preorder()
         if self.right:
-            self.right.print_preorder()
+            self.right.preorder()
 
-    def print_postorder(self):
+    def postorder(self):
         if self.left:
-            self.left.print_postorder()
+            self.left.postorder()
         if self.right:
-            self.right.print_postorder()
+            self.right.postorder()
         print(self.data)
